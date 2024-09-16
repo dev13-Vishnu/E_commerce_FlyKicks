c8dcb48f4b806e31425824b3b96cdf55b3c713f7
Vishnu P S <vishnu13three@gamil.com>
2024-09-16 15:29:00 +0530

    modified wishlist page to accomadate offer prices and added cart count functonality

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -4,39 +4,84 @@ const Cart = require('../models/cartModel');
 const Wishlist = require('../models/wishlistModel');
 const mongoose = require('mongoose')
 
-const loadWishlist = async (req,res) => {
+const loadWishlist = async (req, res) => {
     try {
+        
         const sortQuery = req.query.sort;
         const searchQuery = req.query.q;
         const categoryQuery = req.query.category || '';
         const userId = req.session.user._id;
         const userData = await User.findById(userId);
-        const wishlist = await Wishlist.findOne({ userId }).populate('products.productId');
-        const cart = await Cart.findOne({userId:userId});
+        const wishlist = await Wishlist.findOne({ userId })
+            .populate({
+                path: 'products.productId',
+                populate: [
+                    {
+                        path: 'category', // Populate the category of the product
+                        populate: {
+                            path: 'offer', // Populate the offer within the category
+                        },
+                    },
+                    {
+                        path: 'offer', // Populate the offer directly applied to the product
+                    },
+                ],
+            });
+        const cart = await Cart.findOne({ userId });
 
-        if(!wishlist){
-            return res.render('user/wishlist',{
-                cart,
-                wishlist,
-                userData,
-                searchQuery,
-                sortQuery,
+        if (wishlist) {
+            wishlist.products.forEach(item => {
+                const product = item.productId;
+                let productDiscountedPrice = product.price;
+                let categoryDiscountedPrice = product.price;
 
-            })
+                let productOfferName = '';
+                let categoryOfferName = '';
+
+                // Apply category offer if it exists
+                if (product.category && product.category.offer) {
+                    const categoryDiscount = parseFloat(product.category.offer.discount);
+                    categoryDiscountedPrice = product.price - (product.price * categoryDiscount) / 100;
+                    categoryOfferName = product.category.offer.offerName;
+                }
+
+                // Apply product offer if it exists
+                if (product.offer) {
+                    const productDiscount = parseFloat(product.offer.discount);
+                    productDiscountedPrice = product.price - (product.price * productDiscount) / 100;
+                    productOfferName = product.offer.offerName;
+                }
+
+                // Apply the better offer between product and category
+                if (product.offer && productDiscountedPrice < categoryDiscountedPrice) {
+                    product.discountedPrice = productDiscountedPrice;
+                    product.appliedOffer = productOfferName;
+                } else if (product.category && product.category.offer) {
+                    product.discountedPrice = categoryDiscountedPrice;
+                    product.appliedOffer = categoryOfferName;
+                } else if (product.offer) {
+                    product.discountedPrice = productDiscountedPrice;
+                    product.appliedOffer = productOfferName;
+                } else {
+                    product.discountedPrice = product.price; // No offer applied
+                }
+            });
         }
-  
-        res.render('user/wishlist',{
+
+        res.render('user/wishlist', {
             userData,
             searchQuery,
             sortQuery,
             categoryQuery,
             wishlist,
-            cart
+            cart,
         });
     } catch (error) {
-        console.log('Error from wishlist contorller loadWishlist',error);
+        console.log('Error from wishlist controller loadWishlist:', error);
+        res.status(500).send('Server error');
     }
-}
+};
+
 
 const addToWishlist = async (req, res) => {
     try {
